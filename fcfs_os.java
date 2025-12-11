e8c72f804429fdf4cf1353a26a56b844bbc7243d
Tirtho saha <220150.cse@student.just.edu.bd>
2025-12-11 23:48:25 +0600

    OS fcfs algorithm has been added

---
+++ fcfs_os.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+
+public class fcfs_os {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the process number: ");
+        int n=sc.nextInt();
+        int burst[]=new int[n+1];
+        burst[0]=0;
+        int total=0;
+
+
+        for(int i=1;i<=n;i++){
+            System.out.print("Enter the Brust number of P"+i+":");
+            burst[i]=sc.nextInt();
+            burst[i]=burst[i-1]+burst[i];
+            System.out.println();
+
+       
+        }
+        for(int num:burst){
+            System.out.println("Waiting time for P "+num);
+            total +=num;
+        }
+        System.out.println(total/(burst.length));
+    }
+}
